e81ba017328a9a0a990f4b824838438fcd76c749
gio-oig <araxamiag@gmail.com>
2022-10-19 10:14:36 +0400

    update conmponent change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
+import { useState } from "react";
+
 import RateCard from "src/components/rateCard/rateCard";
 import ResultCard from "./components/resultCard/resultCard";
 
 import "./App.css";
-import { useState } from "react";
-
-enum VIEWS {
-  RATE,
-  RESULT,
-}
 
 function App() {
-  const [currentView, setCurrentView] = useState(VIEWS.RATE);
+  const [isSubmeted, setIsSubmeted] = useState(false);
   const [selectedrating, setSelectedrating] = useState(NaN);
 
   const handleSubmit = () => {
     if (!selectedrating) return;
 
-    setCurrentView(VIEWS.RESULT);
+    setIsSubmeted(true);
   };
 
   return (
     <div className="App">
-      {VIEWS.RATE === currentView ? (
+      {!isSubmeted ? (
         <RateCard
           selectedrating={selectedrating}
           setSelectedrating={setSelectedrating}
